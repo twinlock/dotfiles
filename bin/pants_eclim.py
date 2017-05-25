e2f50fd42ecf96657fe67d825c90c88eb7ee1fd2
Tess Winlock <tess@squareup.com>
2017-05-25 13:56:20 -0700

    fixed up pants eclim thingy

--- bin/pants_eclim.py
+++ bin/pants_eclim.py
@@ -11,30 +11,45 @@ from os import path
 from shutil import which
 
 DEFAULT_PANTS_GEN_DIR=".pants.d/eclipse/eclipse/EclipseGen_eclipse/"
-DEFAULT_CONFIG_DIR=".config/eclim_helper/"
+DEFAULT_CONFIG_DIR=".config/pants_eclim/"
 CONFIG_DIR=path.join(os.path.expanduser('~'),
-        os.environ.get("ECLIM_HELPER_HOME") or DEFAULT_CONFIG_DIR)
+        os.environ.get("PANTS_ECLIM_HOME") or DEFAULT_CONFIG_DIR)
 LOUD_ECHO=which("figlet") or "echo"
 
 @click.group()
 def cli(): pass
 
 @cli.command()
-@click.option("--project", default="sq_java")
-@click.option("--loc", default=DEFAULT_PANTS_GEN_DIR)
-@click.argument("projects", nargs=-1)
-def fix_classpath(project, loc, projects):
-    cleaned_projects = [proj.strip() for proj in list(projects)]
-    project_filename = path.join(loc, project, ".project")
-    classpath_filename = path.join(loc, project, ".classpath")
-    settings_filename = path.join(loc, project, ".settings", "org.eclipse.jdt.ui.prefs")
-    src_re = re.compile("(%s)\..*src\.(main|test)\.java" % ("|".join(cleaned_projects)))
+@click.option("--repo_name", help="name of the repository, doesnt need to match anything")
+@click.option("--eclipse_project_name", help="name of the eclipse project")
+@click.option("--repo_dir", help="directory where the repo is stored")
+@click.argument("targets", nargs=-1)
+def fix_classpath(repo_name, eclipse_project_name, repo_dir, targets):
+    """pants and eclipse (neon at least) are not really compatible, this fixes various issues found
+    in the classpath"""
+    # first check the config to see if the repo exists, if not
+    # add the repo and all the targets using the add_repo method
+    repo_cfg = fetch_or_create_cfg(repo_name, repo_dir, eclipse_project_name, list(targets))
+    if not repo_cfg:
+        return
+    repo_dir = repo_cfg["location"]
+    eclipse_project_name = repo_cfg["eclipse_project_name"]
+    cleaned_targets = repo_cfg["targets"]
+    print("fixing classpath for project %s for targets %s" %
+            (eclipse_project_name, cleaned_targets))
+
+    # if it does exist and there are targets/location, override location/targets with the
+    # ones provided.
+    project_filename = path.join(repo_dir, ".project")
+    classpath_filename = path.join(repo_dir, ".classpath")
+    settings_filename = path.join(repo_dir, ".settings", "org.eclipse.jdt.ui.prefs")
+    src_re = re.compile("(%s)\..*src\.(main|test)\.java" % ("|".join(cleaned_targets)))
 
     # first fix project file, which stores the association of package name to disk location
     project_tree = ElementTree.parse(project_filename)
     resources = project_tree.getroot().find("linkedResources")
     for link in resources.findall("link"):
-        if link.find("name").text in projects:
+        if link.find("name").text in cleaned_targets:
             resources.remove(link)
     project_tree.write(project_filename)
 
@@ -45,7 +60,7 @@ def fix_classpath(project, loc, projects):
     for cpe in classpath_root.findall("classpathentry"):
         if src_re.match(cpe.attrib["path"]):
             junk = cpe.attrib.pop("including", None)
-        elif cpe.attrib["path"] in projects:
+        elif cpe.attrib["path"] in cleaned_targets:
             classpath_root.remove(cpe)
     classpath_tree.write(classpath_filename)
 
@@ -59,20 +74,51 @@ def fix_classpath(project, loc, projects):
             if not [True for prefix in ignored_prefixes if line.startswith(prefix)]:
                 filtered_file.append(line)
         f.writelines(filtered_file)
+    subprocess.run(["vim","-c", "'ProjectRefresh %s'" % eclipse_project_name, "+qall" ], check=True)
 
 @cli.command()
-@click.option("--project_name", default="sq_java")
-@click.argument("projects", nargs=-1)
-def pants_rebuild_eclipse(project_name, projects):
-    cleaned_projects = [proj.strip() for proj in list(projects)]
-    subprocess.run(["pants","compile"] + [proj + "::" for proj in projects], check=True)
-    subprocess.run(["pants","eclipse","--eclipse-project-name=" + project_name]
-            + [proj + "::" for proj in projects], check=True)
-    fix_classpath(project, DEFAULT_PANTS_GEN_DIR, projects)
-    # This wont do anything if your project is already installed
-    subprocess.run(["vim","-c", "'ProjectImport %s'" % DEFAULT_PANTS_GEN_DIR, "+qall" ], check=True)
+@click.option("--repo_name", help="name of the repository, doesnt need to match anything")
+@click.option("--eclipse_project_name", help="name of the eclipse project")
+@click.option("--repo_dir", help="directory where the repo is stored")
+@click.argument("targets", nargs=-1)
+def pants_build_eclipse(repo_name, eclipse_project_name, repo_dir, targets):
+    """buid/rebuild the specified project and add it to eclipse"""
+    repo_cfg = fetch_or_create_cfg(repo_name, repo_dir, eclipse_project_name, list(targets))
+    if not repo_cfg:
+        return
+    repo_dir = repo_cfg["location"]
+    eclipse_project_name = repo_cfg["eclipse_project_name"]
+    cleaned_targets = repo_cfg["targets"]
 
-# Its annoying ot have to retype projects all the time, so all this is to make sure i dont have to
+    print("compiling all pants targets %s" % cleaned_targets)
+    subprocess.run(["pants","compile"] + [proj + "::" for proj in cleaned_targets], check=True)
+    print("creating eclipse project from targets %s" % cleaned_targets)
+    subprocess.run(["pants","eclipse","--eclipse-project-name=" + eclipse_project_name]
+            + [proj + "::" for proj in cleaned_targets], check=True)
+    fix_classpath(repo_name, eclipse_project_name, repo_dir, targets)
+    # This wont do anything if your project is already installed
+    subprocess.run(["vim","-c", "'ProjectImport %s'" % repo_dir, "+qall" ], check=True)
+
+@cli.command()
+@click.option("--repo_name", help="name of the repository, doesnt need to match anything")
+@click.option("--eclipse_project_name", help="name of the eclipse project")
+@click.option("--repo_dir", help="directory where the repo is stored")
+@click.argument("targets", nargs=-1)
+def pants_clean_rebuild_eclipse(repo_name, eclipse_project_name, repo_dir, targets):
+    """same as build eclipse, except delete the project from eclipse and reset pants"""
+    repo_cfg = fetch_or_create_cfg(repo_name, repo_dir, eclipse_project_name, list(targets))
+    if not repo_cfg:
+        return
+    repo_dir = repo_cfg["location"]
+    eclipse_project_name = repo_cfg["eclipse_project_name"]
+    cleaned_targets = repo_cfg["targets"]
+    print("starting from a fresh clean of pants targets, to avoid this use pants_build_eclipse")
+    subprocess.run(["pants","clean-all"], check=True)
+    print("deleteing the old project from eclipse, dont worry this doesn't destroy any code")
+    subprocess.run(["vim","-c", "'ProjectDelete %s'" % repo_dir, "+qall" ], check=True)
+    pants_build_eclipse(repo_name, eclipse_project_name, repo_dir, targets)
+
+# Its annoying ot have to retype targets all the time, so all this is to make sure i dont have to
 # do that all the fucking time
 @cli.command()
 @click.option("--repo_name", default=os.path.basename(os.getcwd()),
@@ -81,7 +127,7 @@ def pants_rebuild_eclipse(project_name, projects):
 def targets(repo_name, repos):
     """list current java targets stored in the ~/.config/eclim_helper/config.yaml file"""
     repos_list = [repo_name] + list(repos)
-    config_fname = init_config(False)
+    config_fname, ignored = init_config(False)
     if not config_fname:
         return
     with open(config_fname, 'r') as f:
@@ -98,39 +144,39 @@ def show_config(repos):
     repos_list = list(repos)
     if repos_list:
         print("Looking for repos %s" % repos_list)
-    config_fname = init_config(False)
+    else:
+        print("Looking up all configs")
+    config_fname, ignored = init_config(False)
     if not config_fname:
         return
     with open(config_fname, 'r') as f:
         cfg = yaml.load(f)
+        current_repo = find_repo(cfg)
+        if current_repo:
+            print("Currently in the %s repo" % current_repo)
         for repo in repos_list or cfg:
             print("repo '%s'" % repo)
-            print("targets: %s" % cfg[repo]["targets"])
+            print("config: %s" % cfg[repo])
 
 @cli.command()
-@click.option("--repo_name", help="name of the repo to add")
+@click.option("--repo_name", help="name of the repository, doesnt need to match anything")
 @click.option("--repo_dir", help="directory where the repo is stored")
+@click.option("--eclipse_project_name", help="name of the eclipse project")
 @click.argument("targets", nargs=-1)
-def add_repo(repo_name, repo_dir, targets):
-    """add a repo to the conifg"""
-    if not repo_dir or not path.isdir(repo_dir):
-        print "must specify at least a valid dir for the repo"
-    if not repo_name:
-        repo_name = path.basename(repo_dir)
-    targets_list = list(targets)
-    config_fname = init_config(False)
-    if not config_fname:
+def add_repo(repo_name, repo_dir, eclipse_project_name, targets):
+    """add/update a repo to the conifg"""
+    if not repo_dir and not repo_name and not eclipse_project_name and not targets:
+        print("must specify at least a valid name for the repo")
+        print_loud("At least try.")
         return
-    with open(config_fname, 'r') as f:
-        cfg = yaml.load(f)
-        if repo_name in cfg:
-            print("Repo %s exists in config use add_targets to add targets" % repo_name)
-            print_loud("Bye~")
-            return
-        cfg[repo_name] = { "location" : repo_dir}
-        if len(targets_list) > 0:
-            cfg[repo_name]["targets"] = targets_list
-
+    if not repo_name:
+        repo_name = path.basename(path.basename(os.getcwd()))
+    if not eclipse_project_name:
+        eclipse_project_name = repo_name
+    if not repo_dir:
+        repo_dir = path.join(DEFAULT_PANTS_GEN_DIR, eclipse_project_name)
+    targets_list = list(targets)
+    fetch_or_create_cfg(repo_name, repo_dir, eclipse_project_name, targets, update=True)
 
 @cli.command()
 @click.option("--repo_name", default=None,
@@ -141,7 +187,7 @@ def add_targets(repo_name, targets):
     targets_list = list(targets)
     if len(targets_list) == 0:
         print("No targets to add!!")
-    config_fname = init_config()
+    config_fname, ignored = init_config()
     with open(config_fname, 'r+') as f:
         cfg = yaml.load(f) or {}
         proper_repo = repo_name or find_repo(cfg)
@@ -161,7 +207,7 @@ def del_targets(repo_name, targets):
     targets_list = list(targets)
     if len(targets_list) == 0:
         print("No targets to remove!!")
-    config_fname = init_config(False)
+    config_fname, ignored = init_config(False)
     if not config_fname:
         return
     with open(config_fname, 'r+') as f:
@@ -197,13 +243,56 @@ def init_config(create=True):
             created = True
         else:
             print("Nothing found at %s that I can read" % config_fname)
-            print("run add_targets first to add a few targets")
+            print("run add_repo first to add a few targets")
             print_loud("GOOD. DAY.")
-            return None
+            return None, None
     return config_fname, created
 
 def print_loud(string):
     subprocess.run([LOUD_ECHO, string], check=True)
+
+def fetch_or_create_cfg(repo_name, repo_dir, eclipse_project_name, targets, update=False):
+    cleaned_targets = [proj.strip() for proj in targets]
+    config_fname, ignored = init_config()
+    if not config_fname:
+        return
+    if repo_dir and not repo_dir.startswith("/"):
+        # make relative paths global
+        repo_dir = path.join(os.getcwd(), repo_dir)
+    repo_cfg = None
+    with open(config_fname, 'r+') as f:
+        cfg = yaml.load(f) or {}
+        proper_repo = repo_name or find_repo(cfg)
+        write = False
+        if proper_repo not in cfg:
+            if not targets:
+                print("repo %s has no targets specified" % proper_repo)
+                print_loud("Adieu")
+                return None
+            cfg[proper_repo] = {
+                    "location" : repo_dir or path.join(os.getcwd(), DEFAULT_PANTS_GEN_DIR),
+                    "eclipse_project_name": eclipse_project_name or sq_java,
+                    }
+            if len(cleaned_targets) > 0:
+                cfg[proper_repo]["targets"] = cleaned_targets
+            write = True
+        elif update:
+            if repo_dir or not cfg[proper_repo]["location"]:
+                cfg[proper_repo]["location"] = repo_dir \
+                        or path.join(os.getcwd(), DEFAULT_PANTS_GEN_DIR)
+                write = True
+            if eclipse_project_name or not cfg[proper_repo]["eclipse_project_name"]:
+                cfg[proper_repo]["eclipse_project_name"] = eclipse_project_name or "sq_java"
+                write = True
+            if len(cleaned_targets) > 0:
+                cfg[proper_repo]["targets"] = cleaned_targets
+                write = True
+        if write:
+            dump = yaml.dump(cfg)
+            f.write(dump)
+        repo_cfg = cfg[proper_repo]
+    return repo_cfg
+
 
 # commands to add:
 #   fresh_start: delete the current project, then recreate it
